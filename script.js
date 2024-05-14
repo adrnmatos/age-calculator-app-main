270951dfc4691378f9896828f6682c3d64ba2159
Adriano Matos <adrnmatos@gmail.com>
2024-05-14 12:52:43 -0400

    returning calculation

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function calculate() {
-    const b_day = document.getElementById('day').value;
-    const b_month = document.getElementById('month').value;
-    const b_year = document.getElementById('year').value;
+    const b_day = document.getElementById('input-day').value;
+    const b_month = document.getElementById('input-month').value;
+    const b_year = document.getElementById('input-year').value;
 
-    let birthday = new Date(b_year, b_month - 1, b_day); // javascript months start from zero
+    let birthday = new Date(b_year, b_month - 1, b_day); // javascript months start at zero
 
     let today = new Date();
 
     const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
     let diffDays = Math.round((Math.abs(today - birthday) / oneDay));
-    alert(diffDays);
+    // alert(diffDays);
 
     let howManyYears = Math.floor(diffDays / 365);
     let howManyMonths = Math.floor((diffDays - (howManyYears * 365)) / 30);
     let remainingDays = diffDays - (365 * howManyYears) - (30 * howManyMonths);
 
-    alert(`${howManyYears} years - ${howManyMonths} months - ${remainingDays} days`);
+    document.getElementById('result-years').innerHTML = howManyYears;
+    document.getElementById('result-months').innerHTML = howManyMonths;
+    document.getElementById('result-days').innerHTML = remainingDays;
+
+    // alert(`${howManyYears} years - ${howManyMonths} months - ${remainingDays} days`);
 }
